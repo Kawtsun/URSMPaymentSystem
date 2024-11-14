fade66e42f5eefad49f83ac2565d327a5a08ee7b
Kawtsun <morpheusjoshua71@gmail.com>
2024-11-14 22:55:51 +0800

    header index

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>URSM Payment System</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <div class="navbar">
+        <div class="logo">
+            <img src="http://www.urs.edu.ph/wp-content/uploads/2023/05/cropped-website-logo.png" alt="urs logo">
+        </div>
+        <div class="buttons">
+            <a href="screens/login.php">Login</a>
+            <a href="screens/register.php" class="action">Register</a>
+        </div>
+    </div>
+    <div class="containers">
+
+    </div>
 </body>
 </html>
--- validate/logout-validate.php
+++ validate/logout-validate.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("Location: ../screens/login.php");
+header("Location: ../index.php");
 
 ?>
